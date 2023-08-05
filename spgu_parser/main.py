8b8e746a01494b142434c2a61d7eb7bb66cffe01
Orex-x <vipboy78038@gmail.com>
2023-08-05 14:54:14 +0300

    add fastapi_jwt_auth

--- spgu_parser/main.py
+++ spgu_parser/main.py
@@ -1,10 +1,38 @@
-from fastapi import FastAPI, Body, status, Request
+from fastapi import FastAPI, Body, status, Request, Depends, HTTPException
 from fastapi.responses import JSONResponse, FileResponse
 from spbu_parser import Parser
+from fastapi_jwt_auth.exceptions import AuthJWTException
+from pydantic import BaseModel
+from fastapi_jwt_auth import AuthJWT
 
 parser = Parser("https://timetable.spbu.ru")
 
 app = FastAPI()
+
+class User(BaseModel):
+    username: str
+    password: str
+
+class Settings(BaseModel):
+    authjwt_secret_key: str = "8r6B2wemSsD4dsEB7vkc0F8veLhSBdofewNhEQjSotQnFyl89uWsiXTzbfaFqSgy"
+    authjwt_decode_algorithms: set = {"HS512"}
+
+@AuthJWT.load_config
+def get_config():
+    return Settings()
+
+@app.exception_handler(AuthJWTException)
+def authjwt_exception_handler(request: Request, exc: AuthJWTException):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={"detail": exc.message}
+    )
+
+@app.get('/expired_time')
+def expiredTime(Authorize: AuthJWT = Depends()):
+    Authorize.jwt_required()
+    return {"hello": "world"}
+
 
 @app.get("/")
 async def main():
