8343e97453b9c35adaf616f6cfdac68eb1e842ba
Orex-x <vipboy78038@gmail.com>
2023-08-16 12:17:03 +0300

    add data storage

---
+++ Skedl.DataStorage/Skedl.DataStorage/Program.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Skedl.DataStorage.Services.DatabaseContexts;
+
+var services = new ServiceCollection();
+
+services.AddDbContext<DatabaseSpbgu>();
+
+services.BuildServiceProvider();
+
+Console.ReadKey();
---
+++ Skedl.DataStorage/Skedl.DataStorage/Services/DatabaseContexts/DatabaseSpbgu.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Skedl.DataStorage.Models;
+
+namespace Skedl.DataStorage.Services.DatabaseContexts;
+
+public class DatabaseSpbgu : DbContext
+{
+    public DbSet<Group> Groups { get; set; }
+
+    public DatabaseSpbgu()
+    {
+        try
+        {
+            Database.EnsureCreated();
+        }
+        catch (Exception)
+        {
+            // ignored
+        }
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql(
+            "host=localhost;port=5432;database=SkedlDataSpbgu;username=postgres;password=123");
+    }
+}
